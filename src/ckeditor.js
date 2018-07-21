d6eb70fc6a60f62c6a7bdc5d575a76bb05b111ef
Yavuz coşar <yavuzcosar@outlook.com>
2018-07-21 19:25:07 +0300

    changes

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -9,19 +9,37 @@ import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classicedi
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Font from '@ckeditor/ckeditor5-font/src/font';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import List from '@ckeditor/ckeditor5-list/src/list';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+
+
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import Link from '@ckeditor/ckeditor5-link/src/link';
-import List from '@ckeditor/ckeditor5-list/src/list';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+
+import Table from '@ckeditor/ckeditor5-table/src/table';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+
+import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave';
+import PendingActions from '@ckeditor/ckeditor5-core/src/pendingactions';
+//import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+import SimpleUpload from 'ckeditor5-simple-upload/src/simpleupload';
+
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -30,46 +48,41 @@ ClassicEditor.builtinPlugins = [
 	Essentials,
 	UploadAdapter,
 	Autoformat,
-	Bold,
-	Italic,
-	BlockQuote,
-	EasyImage,
-	Heading,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
-	Link,
-	List,
-	Paragraph
+	Heading,Font,Alignment,
+	Bold, Italic, Underline, Strikethrough, Code,
+	List,Link,BlockQuote,Paragraph,  
+	Image, ImageCaption, ImageStyle, ImageToolbar, ImageUpload,
+	Table, TableToolbar, 
+	Autosave, PendingActions,
+	SimpleUpload //EasyImage,
 ];
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
-			'heading',
-			'|',
-			'bold',
-			'italic',
-			'link',
-			'bulletedList',
-			'numberedList',
+			'undo','redo','|', 
+			'heading','fontFamily','fontSize','alignment','|', 
+			'bold', 'italic', 'underline', 'strikethrough', 'code','|', 
+			'bulletedList','numberedList','link','blockQuote','|', 
 			'imageUpload',
-			'blockQuote',
-			'undo',
-			'redo'
+			'insertTable', 
 		]
 	},
 	image: {
 		toolbar: [
+			 
+			'imageStyle:alignLeft',
+			'imageStyle:aligncenter',
+			'imageStyle:alignRight',
 			'imageStyle:full',
-			'imageStyle:side',
 			'|',
 			'imageTextAlternative'
 		]
 	},
+	 table: {
+            toolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ]
+        },
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+	language: 'tr'
 };
